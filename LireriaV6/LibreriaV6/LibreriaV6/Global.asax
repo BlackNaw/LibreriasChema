3456fcf0b3f26bde7175cfd8b3c0126e80f442b4
Iván Carrillo <nawtovi@gmail.com>
2017-01-03 11:49:32 +0100

    Nuevo

---
+++ LireriaV6/LibreriaV6/LibreriaV6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibreriaV6.MvcApplication" Language="C#" %>
---
+++ LireriaV6/LibreriaV6/LibreriaV6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibreriaV6.Startup))]
+namespace LibreriaV6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
