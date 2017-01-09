bebe96ce955b771478b13c21cfff0cdc1c777c90
Iván Carrillo <nawtovi@gmail.com>
2017-01-09 09:13:32 +0100

    Terminando e iniciando
    Libreria versión 6 ya acabada e iniciando la libreria 6.1 añadiendo el
    carrito

---
+++ LibreriaV6.1Mia/WebApplication1/Controllers/InicioController.cs
@@ -0,0 +1,27 @@
+﻿using LibreriaV3._1.Comun;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class InicioController : Controller
+    {      
+         
+public ActionResult Inicio()
+        {
+            try
+            {
+                Util.RellenarDictionarySentencias();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return View();
+        }
+
+    }
+}
