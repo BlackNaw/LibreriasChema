bc6bd4ef00acf71149092d276c8e94dc26dba6ca
Iván Carrillo <nawtovi@gmail.com>
2017-01-11 11:31:12 +0100

    Dia 2

--- LibreriaV6.1Mia/WebApplication1/Controllers/TUsuario.cs
+++ LibreriaV6.1Mia/WebApplication1/Controllers/TUsuario.cs
@@ -8,26 +8,30 @@ namespace Libreria_V6.Controllers
 {
     public class TUsuario
     {
+     
+
         public string CodUsuario { get; set; }
         public string Nick { get; set; }
         public string Pass { get; set; }
-        public string rol { get; set; }
+        public string Rol { get; set; }
 
         public TUsuario(string codUsuario, string nick, string pass, string rol)
         {
             CodUsuario = codUsuario;
             Nick = nick;
             Pass = pass;
-            this.rol = rol;
+            Rol = rol;
         }
-        public TUsuario() { }
 
         public TUsuario(string nick, string pass, string rol)
         {
             CodUsuario = Util.GenerarCodigo(new TUsuario().GetType());
             Nick = nick;
             Pass = pass;
-            this.rol = rol;
+            Rol = rol;
         }
+        
+        public TUsuario()
+        {    }
     }
 }
